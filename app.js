c1913749636718adc15b787c32adf5e3062e6d84
coding-with-maaz <microtechhubofficial@gmail.com>
2025-01-10 00:10:54 +0500

    add the frontend connection

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const app = express();
 
 // CORS Configuration
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://goowrite.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin', 'X-Requested-With'],
